cc1d3a86843bcf8d0723ac19c85207ab53f36a12
Christopher D. Zenzel <christopher@arendellekingdom.com>
2019-11-05 17:38:46 -0500

    Work on the DFU Scanner by Service UUID

---
+++ Plugin.BLE.NordicDFU/DFUScanner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace Plugin.XamarinNordicDFU
+{
+    public class DFUScanner
+    {
+        public Func<IDevice, bool> Success { get; set; } = null;
+
+        public async void Scan()
+        {
+            var adapter = CrossBluetoothLE.Current.Adapter;
+            
+            adapter.DeviceDiscovered += async (sender, result) =>
+            {
+                if (Success != null)
+                {
+                    var bStatus = Success.Invoke(result.Device);
+
+                    if (bStatus)
+                    {
+                        await adapter.StopScanningForDevicesAsync();
+                    }
+                }
+                else
+                {
+                    await adapter.StopScanningForDevicesAsync();
+                }
+            };
+
+            await adapter.StartScanningForDevicesAsync(new Guid[]
+            {
+                DFU.DfuServicePublic
+            });
+        }
+    }
+}
